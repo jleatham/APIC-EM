3fb020744898189b3f6056d90308558bc08b7f8e
uruguay96 <j.leatham@gmail.com>
2016-04-15 21:31:51 -0500

    First commit

---
+++ apic_api_explorer.py
@@ -0,0 +1,168 @@
+from apicem_config import * # APIC-EM IP is assigned in apicem_config.pyy
+# Get ticket - function is in apicem_config.py
+ticket = get_X_auth_token()
+headers = {"X-Auth-Token": ticket}
+
+id = ""
+start = ""
+stop = ""
+user_input = ""
+
+
+"""
+This script shouldn't be changed until you complete the Cisco Learning Lab tutorials, which formed the
+basis of this script.
+
+As APIs change or get added upon, you can change/add to the url_lists defined below.
+
+Functions used that I created (all defined in apicem_config.py):
+    build_url_table ()
+        Takes the list of options and associates them with a number in a matrix.  This so you can easily just add
+        to the url_lists defined below, and they will be given a dynamic number in the list
+
+    print_url_table()
+        takes the table created in 'build_url_table()' function and prints it out
+
+    get_json()
+        just a simplified method to get the response from the API and place it in a variable for use
+        Includes some error handling if something goes wrong
+
+    remove_duplicates()
+        takes the result of a regex findall function and removes duplicates
+
+    build_pretty_table()
+        This is an automated way to use the tabulate function defined in tabulate.py.  It will
+        take user input (Copy Paste) from the different JSON API variables, and then build a table
+        with the columns that you select.
+
+        It calls another function I created called tab_json_1_deep_new().  Which is just an easier way
+        to build tables based upon the default tabulate() function defined in tabulte.py
+
+    print_api_json()
+        just prints raw JSON
+
+
+Functions defined by someone else or standard librarys:
+    json.dumps()
+        just takes the raw JSON response and places it into a string.  Usseful for printing raw JSON
+        as well as regex searches.  It is used in my print_api_json function
+
+    re.findall()
+        regex fuction that finds all instances of any regex search parameter you enter.  Must be a string value
+        which is why I convert the JSON to string using the json.dumps() function
+"""
+
+
+while user_input != "0" :
+    print ("\n\n\nThis program allows you to view the response for each API GET call.\n0 exits the program")
+    user_input = input('\n  1  -  Main GET calls\n  2  -  COUNT\n  3  -  RANGE\n=> Make a selection: ')
+    try :
+        if user_input == "1":
+            url_list = [
+                "https://"+apicem_ip+"/api/"+version+"/network-device",
+                "https://"+apicem_ip+"/api/"+version+"/host",
+                "https://"+apicem_ip+"/api/"+version+"/device-credential",
+                "https://"+apicem_ip+"/api/"+version+"/qos/traffic-class",
+                "https://"+apicem_ip+"/api/"+version+"/qos/status"
+
+                ]
+            url_table = build_url_table(url_list)
+            url_selected = url_table
+            print_url_table(url_selected)
+            user_input1 = input('=> Please enter the number of API URL you would like to see: ')
+            for item in url_selected :
+                if user_input1 in item :
+                    url = item[1]
+                    response_json = get_json(url,headers)
+                    print ("URL selected = ", url)
+            user_input2 = input('=> Want to see an organized table or raw json results?\n  1  -  Organized table\n  2  -  Raw JSON\n => ')
+            if user_input2 == "1" :
+                json_list = json.dumps(response_json)
+                json_list_regex = re.findall('\"\w+\":',json_list) # "\w+":   - Finds alphanumeric word inside "": the \" is to escape the quote and search for it literally
+                json_list_pruned = remove_duplicates(json_list_regex)
+                json_list_pruned.sort()
+                for item in json_list_pruned :
+                    if "response" not in item :
+                        print (item)
+                build_pretty_table(response_json)
+            elif user_input2 == "2" :
+                print_api_json(response_json)
+            id = input('=> If you want to drill down by ID, Copy-Paste the ID below, or 0 to exit\n => ')
+            if id != "0" :
+                url_list_id = [
+                    "https://"+apicem_ip+"/api/"+version+"/interface/network-device/"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/network-device/"+id+"/config",
+                    "https://"+apicem_ip+"/api/"+version+"/host/user-id"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/host/"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/discovery/"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/application/"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/category/"+id,
+                    "https://"+apicem_ip+"/api/"+version+"/policy/"+id
+                    ]
+                url_table_id = build_url_table(url_list_id)
+                url_selected = url_table_id
+                print_url_table(url_selected)
+                user_input1 = input('=> Please enter the number of API URL you would like to see: ')
+                for item in url_selected :
+                    if user_input1 in item :
+                        url = item[1]
+                        response_json = get_json(url,headers)
+                        print ("URL selected = ", url)
+                user_input2 = input('=> Want to see an organized table or raw json results?\n  1  -  Organized table\n  2  -  Raw JSON\n => ')
+                if user_input2 == "1" :
+                    json_list = json.dumps(response_json)
+                    json_list_regex = re.findall('\"\w+\":',json_list) # "\w+":   - Finds alphanumeric word inside "": the \" is to escape the quote and search for it literally
+                    json_list_pruned = remove_duplicates(json_list_regex)
+                    json_list_pruned.sort()
+                    for item in json_list_pruned :
+                        if "response" not in item :
+                            print (item)
+                    build_pretty_table(response_json)
+                elif user_input2 == "2" :
+                    print_api_json(response_json)
+        elif user_input == "2":
+            url_list_count = [
+                "https://"+apicem_ip+"/api/"+version+"/reachability-info/count",
+                "https://"+apicem_ip+"/api/"+version+"/host/count",
+                "https://"+apicem_ip+"/api/"+version+"/discovery/count",
+                "https://"+apicem_ip+"/api/"+version+"/application/count",
+                "https://"+apicem_ip+"/api/"+version+"/category/count",
+                "https://"+apicem_ip+"/api/"+version+"/policy/count",
+                "https://"+apicem_ip+"/api/"+version+"/qos/traffic-class/count"
+                ]
+            url_table_count = build_url_table(url_list_count)
+            url_selected = url_table_count
+            print_url_table(url_selected)
+            user_input1 = input('=> Please enter the number of API URL you would like to see: ')
+            for item in url_selected :
+                if user_input1 in item :
+                    url = item[1]
+                    print ("URL selected = ", url)
+                    response_json = get_json(url,headers)
+                    print_api_json(response_json)
+        elif user_input == "3":
+            start = input('=> Start range: ')
+            stop = input('=> Stop range: ')
+            url_list_range = [
+                "https://"+apicem_ip+"/api/"+version+"/reachability-info/"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/host"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/discovery/"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/application/"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/category/count/"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/policy/count/"+start+"/"+stop,
+                "https://"+apicem_ip+"/api/"+version+"/qos/traffic-class/"+start+"/"+stop
+                ]
+            url_table_range = build_url_table(url_list_range)
+            url_selected = url_table_range
+            print_url_table(url_selected)
+            user_input1 = input('=> Please enter the number of API URL you would like to see: ')
+            for item in url_selected :
+                if user_input1 in item :
+                    url = item[1]
+                    print ("URL selected = ", url)
+                    response_json = get_json(url,headers)
+                    print_api_json(response_json)
+        elif user_input == "0" :
+            exit
+    except :
+        print ("Something went wrong, try again")
---
+++ apicem_config.py
@@ -0,0 +1,168 @@
+# Configure APIC-EM IP, username, password, also create a function to obtain a service ticket
+import requests   # We use Python external "requests" module to do HTTP GET query
+import json       # External JSON encoder and decode module
+import sys        # For system-specific functions
+import re	  # Regex funcitonality
+import codecs	  # for testing to see what encoding the JSON results are in
+
+# Please note that you may want install this external module in your working environment
+# We just copy source code in here for the convenient
+from tabulate import tabulate
+
+# It's used to get rid of certificate warning messages when using Python 3.
+# For more information please refer to: https://urllib3.readthedocs.org/en/latest/security.html
+requests.packages.urllib3.disable_warnings() # Disable warning message
+
+# Step 1
+# Change apic-em IP to the one you are using
+apicem_ip = "sandboxapic.cisco.com:9443"
+
+# Step 2
+# Eneter user name and password to get a service ticket
+# If you assign username, password and version here you don't need to pass parameter when calling
+username = "admin"
+password = "C!sc0123"
+version = "v1"
+
+def get_X_auth_token(ip=apicem_ip,uname = username,pword = password):
+    """
+    This function returns a new service ticket.
+    Passing ip, username and password when use as standalone function
+    or overwrite the configuration above.
+    """
+    global version
+
+    # JSON input for the post ticket API request
+    r_json = {
+    "username": uname,
+    "password": pword
+    }
+    # url for the post ticket API request
+    post_url = "https://"+ip+"/api/"+version+"/ticket"
+    # All APIC-EM REST API query and response content type is JSON
+    headers = {'content-type': 'application/json'}
+    # POST request and response
+    try:
+        r = requests.post(post_url, data = json.dumps(r_json), headers=headers,verify=False)
+        # remove '#' if need to print out response
+        # print (r.text)
+
+        # return service ticket
+        return r.json()["response"]["serviceTicket"]
+    except:
+        # Something wrong, cannot get service ticket
+        print ("Status: %s"%r.status_code)
+        print ("Response: %s"%r.text)
+        sys.exit ()
+
+
+
+
+def tab_json_1_deep_new(dump,parent,child):
+    #this function prints out your specified json data in an easy to read table
+    #parent is usually 'response', and child is a list in ['item1', 'item2', 'etc'] form.
+    #Things I learned, the data matrix json created from the raw json dump is in a different format (strings)
+    #than the response_json[parent], which is a matrix
+    child_list = []
+    child_list = child
+    json_raw = []
+    response_json = dump
+    json_raw = response_json[parent]
+    if json != [] :
+        json_list = []
+        for item in json_raw :
+            i = 0
+            temp_list = []
+            while i < len(child_list) :
+                temp_list.append(item[child_list[i]])
+                i += 1
+            json_list.append(temp_list)
+        #This ensures there are no null characters (None), i.e., empty data
+        #THis was done for python3 because the sort function can't deal with it
+        #notice I didnt need to use the child_list variable here, because this is not dealing with json data
+        #it is dealing with my new matrix list i created above.  You can think of the data you are trying
+        #to change as json_list[x][y] = ""
+        for item in json_list :
+            i=0
+            while i < len(item) :
+                if item[i] is None :
+                    item[i] = ""
+                i += 1
+        #print (json_list)
+
+        #just sorts out the first child data alphabetically.  THis can be modified later to be more clever
+        # e.g., selecting what variable you want to sort on, etc
+        json_list.sort()
+        #print (json.dumps(response_json,indent=4))  #This prints out the raw json for testing
+        print (tabulate(json_list, headers=child,tablefmt="rst"))
+        return json_list
+        #print (json_list[12][0])
+    else :
+        print ("Couldn't find data to print")
+
+#End def
+
+
+
+def get_json(url,headers) :
+    try:
+        resp= requests.get(url,headers=headers,verify = False) # The response (result) from "GET /network-device" request
+        status = resp.status_code
+        print("Status: ",status)
+        response_json = resp.json() # Get the json-encoded content from response
+        return response_json
+    except:
+        print ("Something wrong, cannot get network device information")
+        sys.exit()
+    if status != 200:
+        print ("Response status %s,Something wrong !"%status)
+        print (resp.text)
+        sys.exit()
+
+
+
+
+def build_url_table(url_list):
+    i = 1
+    url_table = []
+    for item in url_list :
+        url_table.append([str(i), item])
+        i += 1
+        #print (item)
+    return url_table
+
+
+
+
+def print_url_table(url_table) :
+    for item in url_table:
+        print (item)
+
+def print_api_json(response_json):
+    print ("Raw JSON for testing:\n",json.dumps(response_json,indent = 4))
+
+
+def build_pretty_table(response_json):
+    user_input = ""
+    child = []
+    parent = "response"
+    print ("\n\n\n=======================================================\nSelect your table columns - (Copy-Paste) - one variable at a time. \nWhen you are done, enter \'0\'\n=======================================================")
+    while user_input != "0" :
+        user_input = input('=> Select column: ')
+        if user_input != "0" :
+            child.append(user_input)
+    tab_json_1_deep_new(response_json,parent,child)
+
+
+def get_variable_info(var) :
+    #This is useful for figuring out what type of variable (List,tuple,Dict,string) that the json responses are in
+    #For instance, tabulate only works with dicts, and regex searches only work with strings.
+    print (len(var))
+    #print (codecs.decode(var, 'utf-8'))
+    print (type(var))
+
+def remove_duplicates(seq):
+    #Found on Stack Exchange - fastest way to remove duplicate results from a list
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
